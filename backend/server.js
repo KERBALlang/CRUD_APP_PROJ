a3d4814a00bd270273571ca49b95555cc49fceca
KERBALlang <duecesr4men.lan8@gmail.com>
2023-07-18 22:59:41 -0600

    implemented POST and some CATCHES, remembering simicolons was killing me.

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const port = 3001
 
 
 // main function calls //
+app.use(express.json());
+
+
 app.get('/',(request, response)=>{
     response.send("Hello World")
 })
@@ -16,14 +19,45 @@ app.get('/users',(request, response)=>{
         .then((user)=>{
             response.send(user);
         })
+        .catch(error=>response.status(404).json({message: 'this should work MAYBE'}));
 })
+app.post('/users',(request, response)=>{
+    // console.log('TEST: ', request.body)
+    const testData = request.body;
+    console.log(testData);
+    knex('users').insert(testData)
+        .then(()=>{
+            response.status(200).json({testData});
+    })
+    .catch(error=>response.status(404).json({message: 'this should work MAYBE'}));
+})
+
+
+
+
+// app.put(('/users', (request, response)=>{
+//     knex.('users').where('id', request.params.id).update({})
+// }))
+
+
+
 app.get('/items',(request, response)=>{
     knex.select('*').from('items')
         .then((item)=>{
             response.send(item);
         })
+        .catch(error=>response.status(404).json({message: 'this should work MAYBE'}));
 })
-
+app.post('/items',(request, response)=>{
+    // console.log('TEST: ', request.body)
+    const testData = request.body;
+    console.log(testData);
+    knex('items').insert(testData)
+        .then(()=>{
+            response.status(200).json({testData});
+    })
+    .catch(error=>response.status(404).json({message: 'this should work MAYBE'}));
+})
 
 
 
